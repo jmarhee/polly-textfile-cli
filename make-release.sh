553decc495b582a8732b859004e70e900743cd84
jmarhee <jmarhee@noreply.localhost>
2021-02-05 03:28:15 +0000

    Update 'make-release.sh'

--- make-release.sh
+++ make-release.sh
@@ -8,3 +8,9 @@ curl -X POST "https://git-central.openfunction.co/api/v1/repos/jmarhee/polly-tex
     -H  "accept: application/json" -H  "Content-Type: application/json" \
     -d "{  \"body\": \"Cutting ${TAG} at ${TAG_COMMIT}\",  \"draft\": false,  \"name\": \"${TAG}\",  \"prerelease\": false,  \"tag_name\": \"${TAG}\",  \"target_commitish\": \"${TAG_COMMIT}\"}" \
     -ik
+
+#AUTH_TOKEN=$GITHUB_TOKEN
+#curl \
+#  -X POST "https://api.github.com/repos/jmarhee/polly-textfile-cli/releases" -H "Authorization: token $GITHUB_TOKEN" \
+#  -H "Accept: application/vnd.github.v3+json" \
+#  -d "{  \"body\": \"Cutting ${TAG} at ${TAG_COMMIT}\",  \"draft\": false,  \"name\": \"${TAG}\",  \"prerelease\": false,  \"tag_name\": \"${TAG}\",  \"target_commitish\": \"${TAG_COMMIT}\"}" \
